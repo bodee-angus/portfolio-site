7efd690ee06f308e7678169b84bc51ccd6a6f54e
BoDee Angus <bodee.angus@icloud.com>
2025-09-29 12:19:21 -0600

    Wire App layout with section components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { About } from "./components/About"
+import { Footer } from "./components/Footer"
+import { Header } from "./components/Header"
+import { Hero } from "./components/Hero"
+import { Projects } from "./components/Projects"
+
 export default function App() {
   return (
-    <>
-    </>
+    <div className="size-full">
+      <Header />
+      <main>
+        <Hero />
+        <Projects />
+        <About />
+        <Footer />
+      </main>
+    </div>
   )
 }
